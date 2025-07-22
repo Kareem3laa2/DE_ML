75c7e077720b3ed5e2e7d169d56d6b01d0719f56
kareem3laa2 <kareem3laa2@gmail.com>
2025-07-23 02:35:24 +0300

    Using XGBoost Model and Images of the final arch and dashboard images

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,13 @@ FROM bitnami/spark:3.1.1
 USER root
 
 # Install system & Python dependencies
-RUN apt-get update && \
+RUN sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list && \
+    sed -i 's|http://security.debian.org|http://archive.debian.org/debian-security|g' /etc/apt/sources.list && \
+    apt-get update && \
     apt-get install -y python3 python3-pip openjdk-11-jdk curl && \
     pip3 install --upgrade pip && \
-    pip3 install numpy pandas scikit-learn pyspark findspark && \
-    rm -rf /var/lib/apt/lists/*
+    pip3 install numpy pandas sqlalchemy psycopg2-binary scikit-learn pyspark findspark imbalanced-learn
+
 
 # Add the PostgreSQL JDBC driver
 COPY jars/postgresql-42.7.7.jar /opt/bitnami/spark/jars/
--- spark/model_train.py
+++ spark/model_train.py
@@ -1,31 +1,35 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.ml.feature import StringIndexer, VectorAssembler, StandardScaler, OneHotEncoder
-from pyspark.ml import Pipeline
-from pyspark.ml.classification import RandomForestClassifier
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from imblearn.over_sampling import SMOTE
+from sklearn.compose import ColumnTransformer
+from imblearn.pipeline import Pipeline
+from sqlalchemy import create_engine
 
 
-
-
-# Detect and handle outliers (Winsoization)
 def detect_outliers(df, column):
-    quantiles = df.approxQuantile(column, [0.25, 0.75], 0.05)
-    Q1 = quantiles[0]
-    Q3 = quantiles[1]
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    outliers = df.filter((col(column) < lower_bound) | (col(column) > upper_bound)) 
-    print(f"Number of outliers in {column}: {outliers.count()}")
+    q1 = df[column].quantile(0.25)
+    q3 = df[column].quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
     return lower_bound, upper_bound
 
 def cap_outliers(df, col_name, lower_bound, upper_bound):
-    return df.withColumn(
-        col_name,
-        when(col(col_name) < lower_bound, lower_bound)
-        .when(col(col_name) > upper_bound, upper_bound)
-        .otherwise(col(col_name))
-    )
+    df[col_name] = np.where(df[col_name] < lower_bound, lower_bound, df[col_name])
+    df[col_name] = np.where(df[col_name] > upper_bound, upper_bound, df[col_name])
+    return df
+
+
+
+
+
+
+
 
 
 spark = SparkSession.builder \
@@ -35,46 +39,67 @@ spark = SparkSession.builder \
 
 df = spark.read.parquet("hdfs://namenode:8020/user/telco/cleaned/telco_cleaned.parquet")
 
-
-## Removing Outliers in TotalCharges (Found In the notebook)
-t_lower_bound, t_upper_bound = detect_outliers(df , "totalcharges")
-
-df = cap_outliers(df, "totalcharges", t_lower_bound, t_upper_bound)
+df = df.toPandas()
 
 
-# Data Splitting
-train_data, test_data = df.randomSplit([0.8, 0.2], seed=42)
+lower, upper = detect_outliers(df, "totalcharges")
+df = cap_outliers(df, "totalcharges", lower, upper)
+lower, upper = detect_outliers(df, "monthlycharges")
+df = cap_outliers(df, "monthlycharges", lower, upper)
+lower, upper = detect_outliers(df, "tenure")
+df = cap_outliers(df, "tenure", lower, upper)
 
 
-# Choosing Features based on importance (More Details in the notebook model.ipynb)
-trainCols = train_data.dtypes
-stringCols = [f for (f , v) in trainCols if v == "string" and f not in  ["customerid" , "streamingmovies", "deviceprotection", "streamingtv", "paymentmethod","contranct","onlinebackup"]]
-numericCols = [f for (f, v) in trainCols if ((v == "double") & (f != "churn"))]
 
-strIndexCols = [col+"_index" for col in stringCols]
+string_cols = [col for col in df.columns if df[col].dtype == "object" and col not in ["customerid","gender", "partner", "dependents", "phoneservice", "multiplelines", "phoneservice", "onlinebackup", "deviceprotection", "streamingtv", "streamingmovies"]]
+numeric_cols = [col for col in df.select_dtypes(include=np.number).columns if col not in ["churn","totalcharges"]]
+features = string_cols + numeric_cols
 
-oheCols = [col+"_ohe" for col in stringCols]
 
-allDataCols = oheCols + numericCols 
 
-# Getting the Pipelinee Ready
-indexers = [StringIndexer(inputCol=col, outputCol=col+"_index", handleInvalid='keep') for col in stringCols]
+X = df[features]
+y = df["churn"]
 
-ohe = OneHotEncoder(inputCols=strIndexCols, outputCols=oheCols)
 
-assembler = VectorAssembler(inputCols=allDataCols, outputCol="features_unscaled")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-scaler = StandardScaler(inputCol="features_unscaled", outputCol="features")
 
-rf = RandomForestClassifier(featuresCol="features" , labelCol="churn",predictionCol="churnPrediction")
+preprocessor = ColumnTransformer([
+    ("ohe", OneHotEncoder(handle_unknown="ignore", sparse=False), string_cols),
+    ("sc", StandardScaler(), numeric_cols)
+])
 
-pipeline = Pipeline(stages = indexers +  [ohe ,  assembler  , scaler , rf])
 
-pipeline_model = pipeline.fit(train_data)
+model_pipeline = Pipeline([
+    ("preprocessor", preprocessor),
+    ("smote", SMOTE(random_state=42)),
+    ("GBC", GradientBoostingClassifier(random_state=42))
+])
 
-predictions = pipeline_model.transform(test_data)
 
-predictions_final = predictions.select("customerid", "churn", "churnPrediction")
+model_pipeline.fit(X_train, y_train)
+
+y_pred =model_pipeline.predict_proba(X_test)[:, 1]
+
+threshold = 0.4
+
+y_pred_GBC = (y_pred >= threshold).astype(int)
+
+
+X_test_with_id = X_test.copy()
+X_test_with_id["customerid"] = df.loc[X_test.index, "customerid"]
+X_test_with_id["churn"] = y_test.values
+
+
+X_test_with_id["churn_prediction"] = y_pred_GBC
+
+
+final_df = X_test_with_id[["customerid", "churn", "churn_prediction"]]
+
+print(final_df.head())
+
+
+spark_df = spark.createDataFrame(final_df)
 
 
 PSQL_USERNAME = "postgres"
@@ -85,7 +110,7 @@ PSQL_PASSWORD = "011145"
 
 url = f"jdbc:postgresql://{PSQL_SERVERNAME}:{PSQL_PORTNUMBER}/{PSQL_DBNAME}"
 
-predictions_final.write\
+spark_df.write\
     .format("jdbc")\
     .option("url", url)\
     .option("dbtable", "model.model_predictions")\
@@ -94,3 +119,7 @@ predictions_final.write\
     .option("driver", "org.postgresql.Driver")\
     .mode("overwrite")\
     .save()
+
+
+
+
